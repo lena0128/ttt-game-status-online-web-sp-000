cbeb0ce79460653794e1f396017ffc829e2e7b70
Lena Hou <lenahou2013@gmail.com>
2021-01-26 00:19:51 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,6 +21,8 @@ def won?(board)
       return win_combination
     elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
         return win_combination
+    else position_taken?(board, index)
+      false
   end
  end
 end
