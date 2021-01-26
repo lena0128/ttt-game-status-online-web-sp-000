12f8570a6260a0abecf0052f68d20fefeb426241
Lena Hou <lenahou2013@gmail.com>
2021-01-26 00:11:01 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,9 @@ def won?(board)
     if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
       win_combination
   end
+ end
 end
+
 
 def full?(board)
     board.all? do |board_full|
