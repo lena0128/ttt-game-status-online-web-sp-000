e30ec2f279085acea16f71b292d21ffad9c9ad83
Lena Hou <lenahou2013@gmail.com>
2021-01-26 00:10:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,11 +16,10 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-    WIN_COMBINATIONS.each do |win_combination|
-      if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
-         win_combination
-end
-}
+  WIN_COMBINATIONS.each do |win_combination|
+    if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
+      win_combination
+  end
 end
 
 def full?(board)
